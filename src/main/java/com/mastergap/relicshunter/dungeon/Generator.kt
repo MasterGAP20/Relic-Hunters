ae65d327811d751102b70ec9ef5478b98b32f159
b1n4ry <b1n4ry@guren.ru>
2022-08-28 19:18:24 +0300

    Altered some values on the generator to make it less bloated and not crashing the hell out of the server, also cool relics descriptions

--- src/main/java/com/mastergap/relicshunter/dungeon/Generator.kt
+++ src/main/java/com/mastergap/relicshunter/dungeon/Generator.kt
@@ -6,23 +6,29 @@ import org.bukkit.World
 import org.bukkit.entity.Player
 import org.bukkit.plugin.Plugin
 import kotlin.random.Random
+import kotlin.system.measureNanoTime
+import kotlin.system.measureTimeMillis
+import kotlin.time.measureTime
 
 object Generator {
     val roomNames = listOf("e","es","esw","ew","n","ne","nes","nesw","new","ns","nsw","nw","s","sw","w")
     fun generate(sx: Double, sy: Double, sz: Double, roomCount: Int, theme: String, world: World, plugin: Plugin, sender: Player) {
-        val path = generatePath(roomCount)
-        path.forEach{
-            var possibleRooms = roomNames
-            it.getAdjacent(path).forEach { possibleRooms = possibleRooms.shouldInclude(it) }
-            listOf('n','e','s','w','0').minus(it.getAdjacent(path).toSet()).forEach { possibleRooms = possibleRooms.shouldNotInclude(it) }
-            sender.sendMessage("${it.x}/${it.z}/${it.isCorridor}")
-            sender.sendMessage(path.indexOf(it).toString())
-            sender.sendMessage(it.getAdjacent(path).toString())
-            var room = possibleRooms.random()
-            it.isCorridor = Random.nextBoolean()
-            if(it.isCorridor) room = "corridors/$room"
-            createRoom(plugin,world,sx+it.x,sy,sz+it.z,theme,room)
+        val startTime = measureTimeMillis {
+            val path = generatePath(roomCount)
+            path.forEach{
+                var possibleRooms = roomNames
+                it.getAdjacent(path).forEach { possibleRooms = possibleRooms.shouldInclude(it) }
+                listOf('n','e','s','w','0').minus(it.getAdjacent(path).toSet()).forEach { possibleRooms = possibleRooms.shouldNotInclude(it) }
+                sender.sendMessage("${it.x}/${it.z}/${it.isCorridor}")
+                sender.sendMessage(path.indexOf(it).toString())
+                sender.sendMessage(it.getAdjacent(path).toString())
+                var room = possibleRooms.random()
+                it.isCorridor = Random.nextBoolean()
+                if(it.isCorridor) room = "corridors/$room"
+                createRoom(plugin,world,sx+it.x,sy,sz+it.z,theme,room)
+            }
         }
+        sender.sendMessage("Elapsed time: $startTime")
     }
 
     private fun createRoom(plugin: Plugin,world: World,sx: Double,sy: Double,sz: Double,theme: String,name: String) {
@@ -43,9 +49,11 @@ object Generator {
         var list = mutableListOf<Room>()
         var x = 0.0
         var z = 0.0
+        var direction = (0..3).random()
         list.add(Room(x,z,false))
         while(list.distinct().size < roomCount) {
-            val direction = (0..3).random()
+            val randomRoom = list.random()
+            val changeBranch = (0..10).random()
             var a = x
             var b = z
             when(direction) {
@@ -54,14 +62,22 @@ object Generator {
                 2 -> b+=17
                 3 -> a-=17
             }
+            Bukkit.getConsoleSender().sendMessage("${list.contains(Room(a,b,false))}")
             if(!list.contains(Room(a,b,false))) {
                 x=a
                 z=b
                 list.add(Room(x,z,false))
-                Bukkit.getConsoleSender().sendMessage("Room: $x/$z/false (list size: ${list.distinct().size})")
+                Bukkit.getConsoleSender().sendMessage("Room: $x/$z/false (size: ${list.distinct().size})")
             }
+            if(list.contains(Room(a,b,false))) {
+                x=randomRoom.x
+                z=randomRoom.z
+                //direction = (0..3).random()
+            }
+            if(changeBranch > 8) direction = (0..3).random()
+            list = list.distinct() as MutableList<Room>
         }
-        return list.distinct()
+        return list
     }
 
     private fun Room.getAdjacent(path: List<Room>) : List<Char> {
--- src/main/java/com/mastergap/relicshunter/relics/Relics.kt
+++ src/main/java/com/mastergap/relicshunter/relics/Relics.kt
@@ -1,6 +1,9 @@
 package com.mastergap.relicshunter.relics
 
 import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.Style
+import net.kyori.adventure.text.format.TextColor
+import net.kyori.adventure.text.format.TextDecoration
 import org.bukkit.Material
 import org.bukkit.NamespacedKey
 import org.bukkit.attribute.Attribute
@@ -15,24 +18,31 @@ import org.bukkit.persistence.PersistentDataType
 import java.util.*
 
 object Relics {
-    var masterSword = ItemStack(Material.DIAMOND_SWORD,1)
-    var khopesh = ItemStack(Material.GOLDEN_SWORD,1)
-    var sharpStick = ItemStack(Material.WOODEN_SWORD,1)
-    var archangelSword = ItemStack(Material.NETHERITE_SWORD,1)
+    val style = Style.style(TextColor.fromHexString("#8182d0"),TextDecoration.ITALIC.withState(false))
+
+    val masterSword = ItemStack(Material.DIAMOND_SWORD,1)
+    val masterSwordLore = listOf<Component>(Component.text("Attacking will shoot a wave of energy.", style))
+    val khopesh = ItemStack(Material.GOLDEN_SWORD,1)
+    val khopeshLore = listOf<Component>(Component.text("Just a regular golden sword. Yep, nothing special.", style))
+    val sharpStick = ItemStack(Material.WOODEN_SWORD,1)
+    val sharpStickLore = listOf<Component>(Component.text("Useless.", style))
+    val archangelSword = ItemStack(Material.NETHERITE_SWORD,1)
+    val archangelSwordLore = listOf<Component>(Component.text("Shoots a jet of fire, like a flamethrower.", style))
 
     fun init(){
-        createRelicSword("Master Sword", 10.0, 1521, 2.6, "masterSword", masterSword)
-        createRelicSword("Khopesh",8.0,17,2.6, "khopesh", khopesh)
-        createRelicSword("Sharp Stick",25.0,58,2.6, "sharpSitck", sharpStick)
-        createRelicSword("Archangel's Blade",6.0,1981,2.6, "archangelBlade", archangelSword)
+        createRelicSword("Master Sword", masterSwordLore, 10.0, 1521, 2.6, "masterSword", masterSword)
+        createRelicSword("Khopesh", khopeshLore,.0,17,2.6, "khopesh", khopesh)
+        createRelicSword("Sharp Stick", sharpStickLore,.0,58,2.6, "sharpSitck", sharpStick)
+        createRelicSword("Archangel's Blade", archangelSwordLore,6.0,1981,2.6, "archangelBlade", archangelSword)
     }
 
-    private fun createRelicSword(name: String, dmg: Double, dura: Int, speed: Double, type: String, item: ItemStack){
+    private fun createRelicSword(name: String, lore: List<Component>, dmg: Double, dura: Int, speed: Double, type: String, item: ItemStack){
         val relicMeta = item.itemMeta
         setDamage(dmg,relicMeta)
         setAtkSpeed(speed,relicMeta)
         relicMeta.addItemFlags(ItemFlag.HIDE_ENCHANTS,ItemFlag.HIDE_ATTRIBUTES)
-        relicMeta.displayName(Component.text(name))
+        relicMeta.displayName(Component.text(name, Style.style(TextColor.fromHexString("#6fa7f7"), TextDecoration.ITALIC.withState(false))))
+        relicMeta.lore(lore)
         relicMeta.persistentDataContainer.set(NamespacedKey("mastergap","weapons"), PersistentDataType.STRING, type)
         relicMeta.addEnchant(Enchantment.OXYGEN,1,true)
         if(name == "Archangel's Blade"){
