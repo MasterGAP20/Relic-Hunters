dcc5755391a9fc4334daa9d7811a7ad946d86a3b
b1n4ry <b1n4ry@guren.ru>
2022-08-28 15:36:41 +0300

    IT WORKS!!!!!

--- src/main/java/com/mastergap/relicshunter/dungeon/Generator.kt
+++ src/main/java/com/mastergap/relicshunter/dungeon/Generator.kt
@@ -1,28 +1,25 @@
 package com.mastergap.relicshunter.dungeon
 
-import com.mastergap.relicshunter.dungeon.Generator.getAdjacent
 import com.mastergap.relicshunter.misc.Util
-import org.bukkit.Location
-import org.bukkit.Material
+import org.bukkit.Bukkit
 import org.bukkit.World
 import org.bukkit.entity.Player
 import org.bukkit.plugin.Plugin
 import kotlin.random.Random
 
 object Generator {
-    val roomNames = listOf<String>("e","es","esw","ew","n","ne","nes","nesw","new","ns","nsw","nw","s","sw","w")
+    val roomNames = listOf("e","es","esw","ew","n","ne","nes","nesw","new","ns","nsw","nw","s","sw","w")
     fun generate(sx: Double, sy: Double, sz: Double, roomCount: Int, theme: String, world: World, plugin: Plugin, sender: Player) {
-        //var currentRoom = roomNames.random()
         val path = generatePath(roomCount)
         path.forEach{
-            //createRoom(plugin,world,sx+it.x,sy,sz+it.z,theme,currentRoom)
             var possibleRooms = roomNames
             it.getAdjacent(path).forEach { possibleRooms = possibleRooms.shouldInclude(it) }
-            it.getNotAdjacent(path).forEach { possibleRooms = possibleRooms.shouldNotInclude(it) }
+            listOf('n','e','s','w','0').minus(it.getAdjacent(path).toSet()).forEach { possibleRooms = possibleRooms.shouldNotInclude(it) }
             sender.sendMessage("${it.x}/${it.z}/${it.isCorridor}")
             sender.sendMessage(path.indexOf(it).toString())
             sender.sendMessage(it.getAdjacent(path).toString())
             var room = possibleRooms.random()
+            it.isCorridor = Random.nextBoolean()
             if(it.isCorridor) room = "corridors/$room"
             createRoom(plugin,world,sx+it.x,sy,sz+it.z,theme,room)
         }
@@ -46,52 +43,35 @@ object Generator {
         var list = mutableListOf<Room>()
         var x = 0.0
         var z = 0.0
-        var previous = list
+        list.add(Room(x,z,false))
         while(list.distinct().size < roomCount) {
-            previous = list
-            list.add(Room(x,z,false))
-            val rand = (0..3).random()
-            if(list.distinct() != previous) {
-                when (rand) {
-                    0 -> z -= 17
-                    1 -> x += 17
-                    2 -> z += 17
-                    3 -> x -= 17
-                }
+            val direction = (0..3).random()
+            var a = x
+            var b = z
+            when(direction) {
+                0 -> b-=17
+                1 -> a+=17
+                2 -> b+=17
+                3 -> a-=17
+            }
+            if(!list.contains(Room(a,b,false))) {
+                x=a
+                z=b
+                list.add(Room(x,z,false))
+                Bukkit.getConsoleSender().sendMessage("Room: $x/$z/false (list size: ${list.distinct().size})")
             }
         }
-        list = list.distinct() as MutableList<Room>
-        list.forEach { it.isCorridor = Random.nextBoolean() }
-        return list
+        return list.distinct()
     }
 
     private fun Room.getAdjacent(path: List<Room>) : List<Char> {
         val list = mutableListOf<Char>()
-        //if(path.contains(Location(this.world,this.x,this.y,this.z-17.0))) list.add('n')
-        //if(path.contains(Location(this.world,this.x+17.0,this.y,this.z))) list.add('e')
-        //if(path.contains(Location(this.world,this.x,this.y,this.z+17.0))) list.add('s')
-        //if(path.contains(Location(this.world,this.x-17.0,this.y,this.z))) list.add('w')
-        path.forEach {
-            if(this.noCorridor() == it.noCorridor().add(0.0,-17.0)) list.add('n')
-            if(this.noCorridor() == it.noCorridor().add(17.0,0.0)) list.add('e')
-            if(this.noCorridor() == it.noCorridor().add(0.0,17.0)) list.add('s')
-            if(this.noCorridor() == it.noCorridor().add(-17.0,0.0)) list.add('w')
-        }
-        return list
-    }
-
-    private fun Room.getNotAdjacent(path: List<Room>) : List<Char> {
-        val list = mutableListOf<Char>('n','e','s','w')
-        //if(!path.contains(Location(this.world,this.x,this.y,this.z-17.0))) list.add('n')
-        //if(!path.contains(Location(this.world,this.x+17.0,this.y,this.z))) list.add('e')
-        //if(!path.contains(Location(this.world,this.x,this.y,this.z+17.0))) list.add('s')
-        //if(!path.contains(Location(this.world,this.x-17.0,this.y,this.z))) list.add('w')
-        path.forEach {
-            if(this.noCorridor() == it.noCorridor().add(0.0,-17.0)) list.remove('n')
-            if(this.noCorridor() == it.noCorridor().add(17.0,0.0)) list.remove('e')
-            if(this.noCorridor() == it.noCorridor().add(0.0,17.0)) list.remove('s')
-            if(this.noCorridor() == it.noCorridor().add(-17.0,0.0)) list.remove('w')
-        }
+        var noCorridorPath = path
+        noCorridorPath.forEach { it.isCorridor = false }
+        if(noCorridorPath.contains(Room(this.x,this.z,false).add(0.0,-17.0))) list.add('n')
+        if(noCorridorPath.contains(Room(this.x,this.z,false).add(17.0,0.0))) list.add('e')
+        if(noCorridorPath.contains(Room(this.x,this.z,false).add(0.0,17.0))) list.add('s')
+        if(noCorridorPath.contains(Room(this.x,this.z,false).add(-17.0,0.0))) list.add('w')
         return list
     }
 }
--- src/main/java/com/mastergap/relicshunter/dungeon/Room.kt
+++ src/main/java/com/mastergap/relicshunter/dungeon/Room.kt
@@ -1,10 +1,6 @@
 package com.mastergap.relicshunter.dungeon
 
-class Room(x: Double, z: Double, isCorridor: Boolean) {
-    var x = x
-    var z = z
-    var isCorridor = isCorridor
-
+data class Room(var x: Double, var z: Double, var isCorridor: Boolean) {
     fun noCorridor() : Room {return Room(x,z,false)}
     fun add(a: Double, b: Double) : Room {return Room(x+a,z+b,isCorridor)}
 }
